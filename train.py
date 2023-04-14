55e76ae36e82307e9e4a2cea1676ebcfadd711d6
Syed Nauyan Rashid <nauyan@hotmail.com>
2023-04-14 15:03:17 +0500

    Add files via upload

---
+++ train.py
@@ -0,0 +1,73 @@
+import time
+import torch
+import torchvision
+import timm
+# from torch import nn
+import torch.nn.functional as F
+
+from torchsummary import summary
+from torchvision import transforms
+from tqdm import tqdm
+
+import torch.optim as optim
+import torch.nn as nn
+
+BATCH_SIZE = 64
+EPOCHS = 5
+WORKERS = 48
+IMG_DIMS = (336, 336)
+CLASSES = 10
+
+# MODEL_NAME = 'eva_large_patch14_336.in22k_ft_in22k_in1k'
+MODEL_NAME = 'resnet50d'
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize(IMG_DIMS),
+])
+
+data = torchvision.datasets.CIFAR10('./',
+                                    train=True,
+                                    download=True,
+                                    transform=transform)
+data_loader = torch.utils.data.DataLoader(data,
+                                          batch_size=BATCH_SIZE,
+                                          shuffle=True,
+                                          num_workers=WORKERS)
+
+# model = timm.create_model('eva_giant_patch14_560.m30m_ft_in22k_in1k', pretrained=True)
+model = timm.create_model(MODEL_NAME, pretrained=True, num_classes=CLASSES)
+# model = timm.create_model('resnet50d', pretrained=True, num_classes=256)
+
+# device_ids = [i for i in range(torch.cuda.device_count())]
+# model = nn.DataParallel(model, device_ids = device_ids)
+
+device = torch.device('cuda:0')
+model = model.to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+start = time.perf_counter()
+for epoch in range(EPOCHS):
+    epoch_start_time = time.perf_counter()
+
+    model.train()
+    for batch in tqdm(data_loader, total=len(data_loader)):
+        features, labels = batch[0].to(device), batch[1].to(device)
+
+        optimizer.zero_grad()
+
+        preds = model(features)
+        loss = loss_fn(preds, labels)
+
+        loss.backward()
+        optimizer.step()
+
+    epoch_end_time = time.perf_counter()
+    print(f"Epoch {epoch+1} Time", epoch_end_time - epoch_start_time)
+end = time.perf_counter()
+print("Training Took", end - start)
+
+# print("Model Parameters", model.parameters())
+# print("Model Summary", summary(model, (3,336,336)))
---
+++ train_fsdp.py
@@ -0,0 +1,96 @@
+import os
+import time
+import torch
+import torchvision
+import timm
+# from torch import nn
+import torch.nn.functional as F
+
+from torchsummary import summary
+from torchvision import transforms
+from tqdm import tqdm
+
+import torch.optim as optim
+import torch.nn as nn
+
+from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.distributed as dist
+from torch.utils.data.distributed import DistributedSampler
+
+import functools
+from torch.distributed.fsdp.wrap import (
+    size_based_auto_wrap_policy,
+    enable_wrap,
+    wrap,
+)
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+# from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy
+
+dist.init_process_group("nccl")
+
+local_rank = int(os.environ["LOCAL_RANK"])
+global_rank = int(os.environ["RANK"])
+
+BATCH_SIZE = 256 // int(os.environ["WORLD_SIZE"])
+EPOCHS = 5
+WORKERS = 48
+IMG_DIMS = (336, 336)
+CLASSES = 10
+
+# MODEL_NAME = 'eva_large_patch14_336.in22k_ft_in22k_in1k'
+MODEL_NAME = 'resnet50d'
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize(IMG_DIMS),
+])
+
+data = torchvision.datasets.CIFAR10('./',
+                                    train=True,
+                                    download=True,
+                                    transform=transform)
+sampler = DistributedSampler(data)
+data_loader = torch.utils.data.DataLoader(data,
+                                          batch_size=BATCH_SIZE,
+                                          shuffle=False,
+                                          sampler=sampler,
+                                          num_workers=WORKERS)
+
+torch.cuda.set_device(local_rank)
+torch.cuda.empty_cache()
+# model = timm.create_model('eva_giant_patch14_560.m30m_ft_in22k_in1k', pretrained=True)
+model = timm.create_model(MODEL_NAME, pretrained=True, num_classes=CLASSES)
+# model = timm.create_model('resnet50d', pretrained=True, num_classes=256)
+
+model = model.to('cuda:' + str(local_rank))
+# model = DDP(model, device_ids=[local_rank])
+
+my_auto_wrap_policy = functools.partial(size_based_auto_wrap_policy,
+                                        min_num_params=20000)
+model = FSDP(model, auto_wrap_policy=my_auto_wrap_policy)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+start = time.perf_counter()
+for epoch in range(EPOCHS):
+    epoch_start_time = time.perf_counter()
+
+    model.train()
+    for batch in tqdm(data_loader, total=len(data_loader)):
+        features, labels = batch[0].to(local_rank), batch[1].to(local_rank)
+
+        optimizer.zero_grad()
+
+        preds = model(features)
+        loss = loss_fn(preds, labels)
+
+        loss.backward()
+        optimizer.step()
+
+    epoch_end_time = time.perf_counter()
+    if global_rank == 0:
+        print(f"Epoch {epoch+1} Time", epoch_end_time - epoch_start_time)
+end = time.perf_counter()
+if global_rank == 0:
+    print("Training Took", end - start)
